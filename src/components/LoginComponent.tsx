66e52344d2c1e7a5e3fb6273427de3e79792e1e0
hakyoung <jhy2297@naver.com>
2023-02-02 23:27:43 +0900

    login css

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -51,21 +51,23 @@ const LoginComponent = () => {
           borderRadius: 6,
           backgroundColor: '#c8d6e5',
           flexDirection: 'column',
+          justifyContent: 'center',
           padding: 20,
           width: '40%',
           height: '40%',
         }}
       >
         <h1 style={{ color: '#576574', textAlign: 'center' }}>Plan The Work</h1>
-        <input type="text" value={email} onChange={changeEmail} style={{ margin: 10, padding: 10, border: '1px solid #535c68', borderRadius: 4 }} />
-        <input type="password" value={password} onChange={changePassword} style={{ margin: 10, padding: 10, border: '1px solid #535c68', borderRadius: 4 }} />
-
-        <div style={{ display: 'flex', justifyContent: 'center', fontSize: 12 }}>
-          <span style={{ color: 'white' }}>이메일 / 비밀번호 찾기</span>
-          <span style={{ color: 'white' }}>회원가입</span>
+        <input type="text" value={email} onChange={changeEmail} style={{ margin: 5, padding: 10, border: '1px solid #535c68', borderRadius: 4 }} />
+        <input type="password" value={password} onChange={changePassword} style={{ margin: 5, padding: 10, border: '1px solid #535c68', borderRadius: 4 }} />
+        <div style={{ margin: 5 }}>
+          <button onClick={doTestAll} style={{ width: '100%', padding: 10, borderRadius: 4, backgroundColor: '#2e86de', color: '#fff', cursor: 'pointer' }}>
+            로그인
+          </button>
         </div>
-        <div>
-          <button onClick={doTestAll}>로그인</button>
+        <div style={{ display: 'flex', justifyContent: 'space-between', fontSize: 12 }}>
+          <div style={{ padding: 5, margin: '0px 10px' }}>이메일 / 비밀번호 찾기</div>
+          <div style={{ padding: 5, margin: '0px 10px' }}>회원가입</div>
         </div>
       </div>
     </div>
