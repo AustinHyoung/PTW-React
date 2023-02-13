798aa830dcc1a66943deccdd704b91cfa3f6ba92
hakyoung Jeong <jhy2297@naver.com>
2023-02-13 16:37:45 +0900

    mdi icon+ header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React from 'react';
 import * as S from '../../styles/styles';
 import { useNavigate } from 'react-router-dom';
+import Icon from '@mdi/react';
+import { mdiAccountCircle, mdiMagnify } from '@mdi/js';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -19,13 +21,18 @@ const Header = () => {
   return (
     <>
       <S.HeaderBox>
-        <ul style={{ padding: 6 }}>
+        <ul style={{ padding: 4 }}>
           <li>ptw</li>
           <li>ptw</li>
         </ul>
-        <ul style={{ padding: 6 }}>
-          <li>user</li>
-          <li>user</li>
+        <ul style={{ display: 'flex', alignItems: 'center' }}>
+          <li style={{ display: 'flex', position: 'relative' }}>
+            <Icon path={mdiMagnify} size={1} style={{ position: 'absolute', top: 5, left: 15 }} />
+            <input style={{ border: '1px solid black', borderRadius: 4, margin: '0 8px', padding: '0 8px 0 40px', height: 32, width: 200, fontSize: 14 }} />
+          </li>
+          <li style={{ display: 'flex', padding: 4 }}>
+            <Icon path={mdiAccountCircle} size={1.5} color="blue" />
+          </li>
         </ul>
       </S.HeaderBox>
     </>
