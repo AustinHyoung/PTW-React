bab5f3fb9917ce9207528a578ae33a23a02fe42c
hakyoung Jeong <jhy2297@naver.com>
2023-02-14 17:56:10 +0900

    dnd example

---
+++ src/components/dnd/DragAndDrop.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Kanban from './Kanban';
+
+const DragAndDrop = () => {
+  return (
+    <>
+      <Kanban />
+    </>
+  );
+};
+
+export default DragAndDrop;
