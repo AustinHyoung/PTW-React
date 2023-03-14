d5109a7a6844272f041b3dd42e311c445d31757a
Hakyoung <jhy2297@naver.com>
2023-03-14 18:40:31 +0900

    rollback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,10 @@ import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import * as S from '../styles/styles';
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/reducer';
-import axios from 'axios';
-import { useQuery } from 'react-query';
-
-const fetchData = async () => {
-  const { data } = await axios.get('http://localhost:8080/apis/boardlist', { withCredentials: true });
-  return data;
-};
 
 const Home = () => {
-  const info = useSelector((state: RootState) => state.persistedReducer.data);
-
-  const { isLoading, data, error } = useQuery('boardList', fetchData);
-
+  const data = useSelector((state: RootState) => state.persistedReducer.data);
   console.log(data);
-
   return (
     <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
       <div
@@ -35,12 +24,6 @@ const Home = () => {
           <Link to="/board">
             <S.BoardItem>1번째 보드</S.BoardItem>
           </Link>
-          <S.BoardItem>1번째 보드</S.BoardItem>
-          <S.BoardItem>1번째 보드</S.BoardItem>
-          <S.BoardItem>1번째 보드</S.BoardItem>
-          <S.BoardItem>1번째 보드</S.BoardItem>
-          <S.BoardItem>1번째 보드</S.BoardItem>
-          <S.BoardItem>1번째 보드</S.BoardItem>
           <S.BoardItem>1번째 보드</S.BoardItem>
           <S.BoardItem>1번째 보드</S.BoardItem>
           <S.BoardItem>+ 새로 만들기</S.BoardItem>
