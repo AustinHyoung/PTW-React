29523d4f28f11438fa8f7f92b904c159d23bd305
Hakyoung <jhy2297@naver.com>
2023-03-22 18:49:00 +0900

    reducer override

--- src/reducer/list.ts
+++ src/reducer/list.ts
@@ -1,7 +1,3 @@
-import { persistReducer } from 'redux-persist';
-import { combineReducers } from 'redux';
-import storage from 'redux-persist/lib/storage';
-
 const SET_LIST = 'storage/SET_LIST' as const;
 
 export type ListProps = {
@@ -14,22 +10,17 @@ export const setList = (diff: ListProps) => ({
   payload: diff,
 });
 
-export const persistConfig = {
-  key: 'list',
-  storage,
-};
-
 type ListAction = ReturnType<typeof setList>;
 
 type ListState = {
   data: ListProps | null;
 };
 
-const initialState: ListState = {
+export const initialState: ListState = {
   data: null,
 };
 
-const list = (state: ListState = initialState, action: ListAction) => {
+export const listReducer = (state: ListState = initialState, action: ListAction) => {
   switch (action.type) {
     case SET_LIST:
       return {
@@ -40,5 +31,3 @@ const list = (state: ListState = initialState, action: ListAction) => {
       return state;
   }
 };
-
-export const persistedListReducer = persistReducer(persistConfig, list);
